674eec0774da54c672e7e8816863cf697c27d222
Patrick Slagle <slaglebagel17@gmail.com>
2018-11-25 23:13:53 -0800

    created a snapshot test for footer-nav component

--- app/components/star-wars/people.test.js
+++ app/components/star-wars/people.test.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import App from './app.jsx'
-// import renderer from 'react-test-renderer'
-import { shallow, configure } from 'enzyme';
+import React from 'react';
 import Adapter from 'enzyme-adapter-react-15';
+import { shallow, configure } from 'enzyme';
+import App from './app.jsx';
+import FooterNav from './footer-nav.jsx';
 
 configure({ adapter: new Adapter() });
 
 test('App component renders properly', () => {
   const wrapper = shallow(<App />);
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('FooterNav component renders properly', () => {
+  const fetchPeopleMock = jest.fn();
+  const wrapper = shallow(
+    <FooterNav 
+      fetchPeople={fetchPeopleMock}
+      loadingPeople={false}
+      next="https://swapi.co/api/people/?page=3"
+      prev="https://swapi.co/api/people/?page=1"
+    />,
+  );
   expect(wrapper).toMatchSnapshot();
 });
