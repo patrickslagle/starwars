a65c8d33b9781c1fe34f898f4de3086e2f9ba636
Patrick Slagle <slaglebagel17@gmail.com>
2018-11-25 23:17:20 -0800

    added test for search-people component

--- app/components/star-wars/__tests__/starwars.test.js
+++ app/components/star-wars/__tests__/starwars.test.js
@@ -3,6 +3,7 @@ import Adapter from 'enzyme-adapter-react-15';
 import { shallow, configure } from 'enzyme';
 import App from '../app.jsx';
 import FooterNav from '../footer-nav.jsx';
+import SearchPeople from '../search-people.jsx';
 
 configure({ adapter: new Adapter() });
 
@@ -20,6 +21,14 @@ test('FooterNav component renders properly', () => {
       next="https://swapi.co/api/people/?page=3"
       prev="https://swapi.co/api/people/?page=1"
     />,
+  );
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('SearchPeople component renders properly', () => {
+  const fetchPeopleMock = jest.fn();
+  const wrapper = shallow(
+    <SearchPeople fetchPeople={fetchPeopleMock} />,
   );
   expect(wrapper).toMatchSnapshot();
 });
