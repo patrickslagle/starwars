98cbef3711afa6088a814f138dea8e4512f07751
Patrick Slagle <slaglebagel17@gmail.com>
2018-11-25 23:10:50 -0800

    updated search component

---
+++ app/components/star-wars/search-people.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SearchPeople = ({ fetchPeople }) => {
+  // fetches Star Wars characters that contain the text in the input
+  const autocomplete = (event) => {
+    fetchPeople(`https://swapi.co/api/people/?search=${event.target.value}`);
+  };
+
+  return (
+    <input
+      id="search-bar"
+      placeholder="Search Star Wars Characters..."
+      onChange={autocomplete}
+    />
+  );
+};
+
+SearchPeople.propTypes = {
+  fetchPeople: PropTypes.func.isRequired,
+};
+
+export default SearchPeople;
